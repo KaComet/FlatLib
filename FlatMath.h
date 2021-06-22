a03e6ae0976a225e11cbbb7e15daf2bdd050808b
Kacomet <Catfish5994@sbcglobal.net>
2021-06-22 17:34:27 -0500

    Added a binary search system that returns the found item's index. I have no clue why this is not somewhere in std.

--- FlatMath.h
+++ FlatMath.h
@@ -45,6 +45,39 @@ namespace flat {
     IntegerCoordinate makeUnit(const IntegerCoordinate &cord);
 
     bool cordIsUnidirectional(const IntegerCoordinate &cord);
+
+    const std::size_t BINSEARCH_NOT_FOUND = -1;
+
+    template<class D, class C>
+    std::size_t binarySearch(std::size_t startInc, std::size_t endInc, C &c, D &d) {
+        if (startInc > endInc)
+            return BINSEARCH_NOT_FOUND;
+
+        std::size_t currentIndex = ((endInc - startInc) / 2) + startInc;
+        std::size_t currentUpperBoundInc = endInc;
+        std::size_t currentLowerBoundInc = startInc;
+        std::size_t lastIndex = -1;
+
+        while (currentIndex != lastIndex) {
+            lastIndex = currentIndex;
+            auto q = c[currentIndex] <=> d;
+            // Another missed opportunity from C++20. Am I the only one that likes switch statements?
+            if (q == std::strong_ordering::greater) {
+                currentUpperBoundInc = currentIndex;
+                currentIndex = ((currentIndex - currentLowerBoundInc) / 2) + currentLowerBoundInc;
+            } else {
+                if (q == std::strong_ordering::less) {
+                    currentLowerBoundInc = currentIndex;
+                    currentIndex = ((currentUpperBoundInc - currentIndex) / 2) + currentIndex;
+                } else {
+                    return currentIndex;
+                }
+            }
+
+        }
+
+        return BINSEARCH_NOT_FOUND;
+    }
 }
 
 #endif //TILESETVIEWER_FLATMATH_H
